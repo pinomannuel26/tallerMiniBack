1082b97f2efa1001ca1d83d8bb4146e571f59eac
Mannuel F. Granoble Pino <57782912+pinomannuel26@users.noreply.github.com>
2023-11-22 00:07:46 -0500

    change

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router(`${__dirname}/data/data.json`);
+const router = jsonServer.router(`${__dirname}/data/db.json`);
 const middlewares = jsonServer.defaults();
 
 const port = process.env.PORT || 3000;
